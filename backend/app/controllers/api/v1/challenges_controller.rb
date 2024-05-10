26009b575bc4ed15bb47e49e5f35e037d4804c19
zehan khan <zehan9211@gmail.com>
2024-05-11 02:33:28 +0530

    add controllers and definations

---
+++ backend/app/controllers/api/v1/challenges_controller.rb
@@ -0,0 +1,27 @@
+module Api
+    module V1
+        class Challenges
+
+            def index
+                # show all challenges
+            end
+
+            def create
+                # create single challenges
+            end
+
+            def show
+                # show single challenges
+            end
+
+            def update
+                # update single challenges
+            end
+
+            def destroy
+                # delete single challenges
+            end
+    
+        end
+    end
+end
