41eda635809522e7a9bed764f38087ea9ab13cfd
zehan khan <zehan9211@gmail.com>
2024-05-12 01:02:48 +0530

    create all crud operations

--- backend/app/controllers/api/v1/challenges_controller.rb
+++ backend/app/controllers/api/v1/challenges_controller.rb
@@ -11,7 +11,7 @@ module Api
             # POST /api/v1/challenges
             def create
                 # create single challenges
-                challenge = Challenge.new(challenges_params)
+                challenge = Challenge.new(challenge_params)
                 if challenge.save
                     render json: {
                         message: "Challenge added successfully",
@@ -21,28 +21,60 @@ module Api
                     render json: {
                         message: "Failed to add challenge",
                         data: challenge.errors                        
-                    }
+                    }, status: :internal_server_error
                 end
             end
 
             # GET /api/v1/challenges/:id
             def show
                 # show single challenges
+                @challenge = Challenge.find_by(id: params[:id]) 
+                if @challenge
+                    render json: {
+                        message: "challenge found",
+                        data: @challenge
+                    } 
+                else
+                    render json: {
+                        message: "Challenge not found",
+                    }, status: :not_found    
+                end     
             end
 
             # PUT/PATCH /api/v1/challenges/:id
             def update
                 # update single challenges
+                @challenge = Challenge.find(params[:id])
+                if @challenge.update(challenge_params)
+                    render json: {
+                        message: "Challenge update succesfully",
+                        data: @challenge
+                    } 
+                else
+                    render json: {
+                        message: "Failed to update Challenge",
+                    }, status: :internal_server_error   
+                end   
             end
 
             # DELETE /api/v1/challenges/:id
             def destroy
                 # delete single challenges
+                @challenge = Challenge.find(params[:id])
+                if @challenge.destroy
+                    render json: {
+                        message: "Challenge delete succesfully",
+                    } 
+                else
+                    render json: {
+                        message: "Failed to delete Challenge",
+                    }, status: :internal_server_error
+                end
             end
 
             private
 
-            def challenges_params
+            def challenge_params
                 params.require(:challenge).permit(:title, :description, :start_date, :end_date)
             end
         end
