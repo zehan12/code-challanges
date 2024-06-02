e425fe1e04ecda127654eb1f2a3404493b4366b7
zehan khan <zehan9211@gmail.com>
2024-06-02 14:06:47 +0530

    bugfix: fix  issue with db drop, create and migrate

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_01_203927) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_01_205707) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
